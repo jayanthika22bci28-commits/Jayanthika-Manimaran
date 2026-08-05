757ec912b81cae2e336df26986fb93db5f2ef443
jayanthika22bci28-commits <jayanthika22_bci28@mepcoeng.ac.in>
2026-08-05 14:45:33 +0530

    Create ternary.c

---
+++ ternary.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+  2 int main()
+  3 {
+  4    int number;
+  5    printf("enter a number:");
+  6    scanf("%d",&number);
+  7    (number%6==0)?
+  8    printf("%d is  divisible by 6\n",number):
+  9    printf("%d is not  divisible by 6\n",number);
+ 10    return 0;
+ 11 }
+~
